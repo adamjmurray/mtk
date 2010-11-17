52db886ff3c5f5ae6688c59ff9b1a300921f1f49
Adam Murray <adamjmurray@gmail.com>
2010-11-17 00:47:56 -0800

    refactored tests

--- spec/kreet/value_spec.rb
+++ spec/kreet/value_spec.rb
@@ -1,16 +1,9 @@
 require 'spec_helper'
 module Kreet
-  
-  describe Value do
-  
-    let(:value) { 70.5 }
-    subject { Value.new(value) }
-    let(:lo) { Value.new(0.0) }
-    let(:hi) { Value.new(1.0) }
-    let(:subjects) { [lo, hi] }
-    
+
+  shared_examples_for "any Value" do
+
     it 'should have Comparable methods' do
-      Value.should include Comparable      
       lo.should      < hi
       hi.should_not  > hi
       lo.should     <= lo
@@ -22,7 +15,7 @@ module Kreet
       hi.should      > lo
       lo.should_not  > hi
     end
-    
+
     describe '#value' do
       it 'is the value the object was constructed with' do
         subject.value.should == value
@@ -32,28 +25,27 @@ module Kreet
     describe '#to_f' do
       it 'is the value as a float' do
         val = Rational(3,5)
-        Value.new( val ).to_f.should == val.to_f
+        subject.class.new( val ).to_f.should == val.to_f
       end
     end
-    
+
     describe '#to_i' do
       it 'is the value rounded to the nearest int' do
         subject.to_i.should == value.round
       end
     end
-    
+
     describe '#==' do
       it 'is true if the values are equal' do
-        for value in [-10, 0, 5, 100]
-          Value.new( value ).should == Value.new( value.to_f )
-        end
+        lo.should == lo
+        hi.should == hi
       end
       it 'is false if the values are not equal' do
-        Value.new( 0 ).should_not == Value.new( 0.001 )
-        Value.new( -1 ).should_not == Value.new( 1 )        
+        lo.should_not == hi
+        hi.should_not == lo
       end
     end
-    
+
     describe '#<=>' do
       it "is the receiver's value <=> the argument's value" do
         for s1 in subjects
@@ -63,31 +55,66 @@ module Kreet
         end
       end
     end 
-  
+
     describe '#+' do
       it 'produces a Value with the value of the sum' do
-        ( subject + 3 ).should == Value.new( subject.value + 3 )
+        ( subject + 3 ).value.should == subject.value + 3
       end    
+      it 'produces a type of Value with the same class as the receiver' do
+        (subject + 1.5).should be_a subject.class
+      end
     end
 
     describe '#-' do
       it 'produces a Value with the value of the difference' do
-        ( subject + 3 ).should == Value.new( subject.value + 3 )
+        ( subject - 3 ).value.should == subject.value - 3
       end    
+      it 'produces a type of Value with the same class as the receiver' do
+        (subject - 1.5).should be_a subject.class
+      end      
     end
-    
+
     describe '#*' do
       it 'produces a Value with the value of the product' do
-        ( subject * 3 ).should == Value.new( subject.value * 3 )
-      end      
+        ( subject * 3 ).value.should == subject.value * 3
+      end
+      it 'produces a type of Value with the same class as the receiver' do
+        (subject * 1.5).should be_a subject.class
+      end            
     end
 
     describe '#/' do
       it 'produces a Value with the value of the product' do
-        ( subject * 3 ).should == Value.new( subject.value * 3 )
-      end      
+        ( subject / 3 ).value.should == subject.value / 3
+      end
+      it 'produces a type of Value with the same class as the receiver' do
+        (subject / 1.5).should be_a subject.class
+      end            
     end
-  
+
+    describe '%/' do
+      it 'produces a Value with the value of the remainder' do
+        ( subject % 3 ).value.should == subject.value % 3
+      end
+      it 'produces a type of Value with the same class as the receiver' do
+        (subject % 1.5).should be_a subject.class
+      end            
+    end
+
   end
-  
+
+  describe Value do
+
+    let(:value) { 9.99 }
+    subject { Value.new(value) }
+    let(:lo) { Value.new(0.0) }
+    let(:hi) { Value.new(1.0) }
+    let(:subjects) { [lo, hi] }
+
+    it_behaves_like "any Value"
+    
+    describe 'of' do it end
+      
+  end
+
 end
--- spec/kreet/velocity_spec.rb
+++ spec/kreet/velocity_spec.rb
@@ -1,4 +1,10 @@
 require 'spec_helper'
+begin # this block seems to be needed to make things work in textmate without breaking rake
+  ensure_shared_example_group_name_not_taken "any Value"   
+  require 'kreet/value_spec'
+rescue 
+end
+
 module Kreet
 
   describe Velocity do
@@ -8,85 +14,8 @@ module Kreet
     let(:lo) { Velocity.new(0.0) }
     let(:hi) { Velocity.new(1.0) }
     let(:subjects) { [lo, hi] }
-    
-    it 'should have Comparable methods' do
-      Velocity.should include Comparable      
-      lo.should      < hi
-      hi.should_not  > hi
-      lo.should     <= lo
-      lo.should     <= hi
-      hi.should_not <= lo
-      lo.should     >= lo
-      hi.should     >= lo
-      lo.should_not >= hi
-      hi.should      > lo
-      lo.should_not  > hi
-    end
-    
-    describe '#value' do
-      it 'is the value the object was constructed with' do
-        subject.value.should == value
-      end
-    end
 
-    describe '#to_f' do
-      it 'is the value as a float' do
-        val = Rational(3,5)
-        Velocity.new( val ).to_f.should == val.to_f
-      end
-    end
-    
-    describe '#to_i' do
-      it 'is the value rounded to the nearest int' do
-        subject.to_i.should == value.round
-      end
-    end
-    
-    describe '#==' do
-      it 'is true if the values are equal' do
-        for value in [-10, 0, 5, 100]
-          Velocity.new( value ).should == Velocity.new( value.to_f )
-        end
-      end
-      it 'is false if the values are not equal' do
-        Velocity.new( 0 ).should_not == Velocity.new( 0.001 )
-        Velocity.new( -1 ).should_not == Velocity.new( 1 )        
-      end
-    end
-    
-    describe '#<=>' do
-      it "is the receiver's value <=> the argument's value" do
-        for s1 in subjects
-          for s2 in subjects
-            ( s1 <=> s2 ).should == ( s1.value <=> s2.value )
-          end
-        end
-      end
-    end 
-  
-    describe '#+' do
-      it 'produces a Velocity with the value of the sum' do
-        ( subject + 3 ).should == Velocity.new( subject.value + 3 )
-      end    
-    end
-
-    describe '#-' do
-      it 'produces a Velocity with the value of the difference' do
-        ( subject + 3 ).should == Velocity.new( subject.value + 3 )
-      end    
-    end
-    
-    describe '#*' do
-      it 'produces a Velocity with the value of the product' do
-        ( subject * 3 ).should == Velocity.new( subject.value * 3 )
-      end      
-    end
-
-    describe '#/' do
-      it 'produces a Velocity with the value of the product' do
-        ( subject * 3 ).should == Velocity.new( subject.value * 3 )
-      end      
-    end
+    it_behaves_like "any Value"
   
   end
 end
