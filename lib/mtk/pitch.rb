28aec066adfada177f49adc24c27d5ca3c99bf92
Adam Murray <adamjmurray@gmail.com>
2010-11-18 00:57:57 -0800

    added @offset to pitch to support the continuous frequency spectrum, and started modeling cents (1/100 of a semitone)

--- lib/mtk/pitch.rb
+++ lib/mtk/pitch.rb
@@ -1,13 +1,14 @@
 module MTK
 
-  # A frequency specified by a {PitchClass} and an integer octave.
+  # A logarithmic representation of frequency 
+  # specified by a {PitchClass}, an integer octave, and an offset in fractional semitones.
   
   class Pitch
 
-    attr_reader :pitch_class, :octave
+    attr_reader :pitch_class, :octave, :offset
 
-    def initialize( pitch_class, octave )
-      @pitch_class, @octave = pitch_class, octave
+    def initialize( pitch_class, octave, offset=0 )
+      @pitch_class, @octave, @offset = pitch_class, octave, offset
     end    
     
     def self.[]( *args )
@@ -26,6 +27,7 @@ module MTK
     end
     
     def self.from_s( s )
+      # TODO: update to handle offset
       s = s.to_s
       s = s[0].upcase + s[1..-1].downcase # normalize name
       if s =~ /^([A-G](#|##|b|bb)?)(-?\d+)$/
@@ -37,19 +39,46 @@ module MTK
       end
     end
     
-    def self.from_i( i )
-      i = i.to_i
+    def self.from_f( f )
+      i, offset = f.floor, f%1  # split into int and fractional part
       pitch_class = PitchClasses::PITCH_CLASSES[i % 12]
       octave = i/12 - 1
-      new( pitch_class, octave )
+      new( pitch_class, octave, offset )      
+    end      
+    
+    def self.from_i( i )
+      self.from_f( i ) 
     end    
     
-    def to_i
-      @int_value ||= @pitch_class.to_i + 12*(@octave+1)
+    def self.from_cents( cents )
+      self.from_f( cents/100.0 )
+    end
+    
+    # The numerical value of this pitch without the offset
+    def base_value
+      @base_value ||= @pitch_class.to_i + 12*(@octave+1)
+    end
+
+    # The numerical value of this pitch
+    def to_f
+      @float_value ||= base_value + @offset
     end 
+
+    # The numerical value for the nearest semitone
+    def to_i
+      @int_value ||= to_f.round
+    end
+    
+    def to_cents
+      to_f * 100
+    end
+    
+    def offset_in_cents
+      @offset * 100
+    end
     
     def to_s
-      "#@pitch_class#@octave"
+      @s ||= "#@pitch_class#@octave" + if @offset.zero? then '' else "+#{offset_in_cents}cents" end
     end
     
     def ==( other )
@@ -58,13 +87,13 @@ module MTK
     end
     
     def +( interval )
-      self.class.from_i( to_i + interval.to_i )
+      self.class.from_f( to_f + interval.to_f )
     end
       
     def -( interval )
-      self.class.from_i( to_i - interval.to_i )
+      self.class.from_f( to_f - interval.to_f )
     end
-        
+              
   end
   
 end
--- spec/mtk/pitch_spec.rb
+++ spec/mtk/pitch_spec.rb
@@ -8,6 +8,7 @@ require 'spec_helper'
     let(:lowest)   { Pitch[c,-1] }
     let(:highest)  { Pitch[g, 9] }   
     let(:subjects) { [middle_c, lowest, highest] }   
+    let(:middle_c_and_50_cents) { Pitch.new(c,4,0.5) }
 
     describe '#pitch_class' do
       it "is the pitch class of the pitch" do
@@ -21,10 +22,40 @@ require 'spec_helper'
       end
     end
 
+    describe '#offset' do
+      it 'is the third argument the constructor' do
+        Pitch.new( c, 4, 0.6 ).offset.should == 0.6
+      end
+      it 'defaults to 0' do
+        Pitch.new( c, 4 ).offset.should == 0
+      end
+    end
+    
+    describe '#offset_in_cents' do
+      it 'is #offset * 100' do
+        middle_c_and_50_cents.offset_in_cents.should == middle_c_and_50_cents.offset * 100
+      end
+    end
+
     describe '.from_i' do
       it( "converts 60 to middle C" ) { Pitch.from_i(60).should == middle_c }
       it( "converts 0 to C at octave -1" ) { Pitch.from_i(0).should == lowest }
       it( "converts 127 to G at octave 9" ) { Pitch.from_i(127).should == highest }      
+    end
+
+    describe '.from_f' do
+      it "converts 60.5 to middle C with a 0.5 offset" do 
+        p = Pitch.from_f( 60.5 )
+        p.pitch_class.should == c
+        p.octave.should == 4
+        p.offset.should == 0.5
+      end
+    end
+    
+    describe '.from_cents' do
+      it "acts like .from_f with the argument multiplied by 100" do
+        Pitch.from_cents( 6050 ).should == Pitch.from_f( 60.5 )
+      end
     end
     
     describe '.from_s' do
@@ -41,11 +72,27 @@ require 'spec_helper'
         Pitch[3].should == Pitch.from_i(3)
       end
     end
+    
+    describe '#to_f' do
+      it "is 60.5 for middle C with a 0.5 offset" do
+        middle_c_and_50_cents.to_f.should == 60.5
+      end
+    end
 
     describe '#to_i' do
       it( "is 60 for middle C" ) { middle_c.to_i.should == 60 }
       it( "is 0 for the C at octave -1" ) { lowest.to_i.should == 0 }
       it( "is 127 for the G at octave 9" ) { highest.to_i.should == 127 }
+      it "rounds to the nearest integer (the nearest semitone value) when there is an offset" do
+        Pitch.new( c, 4, 0.4 ).to_i.should == 60
+        Pitch.new( c, 4, 0.5 ).to_i.should == 61
+      end
+    end
+    
+    describe '#to_cents' do
+      it "is #to_f * 100" do
+        middle_c_and_50_cents.to_cents.should == middle_c_and_50_cents.to_f * 100
+      end
     end
 
     describe '#==' do
@@ -64,17 +111,26 @@ require 'spec_helper'
           pitch.to_s.should == pitch.pitch_class.name + pitch.octave.to_s
         end
       end
+      it "should include the offset_in_cents when the offset is not 0" do
+        middle_c_and_50_cents.to_s.should == "C4+50.0cents"
+      end
     end
     
     describe '#+' do
       it 'adds the integer value of the argument and #to_i' do
         (middle_c + 2).should == Pitch.from_i( 62 )
       end
+      it 'handles offsets' do
+        pending
+      end
     end
 
     describe '#-' do
       it 'subtracts the integer value of the argument from #to_i' do
         (middle_c - 2).should == Pitch.from_i( 58 )
+      end
+      it 'handles offsets' do
+        pending
       end
     end
     
