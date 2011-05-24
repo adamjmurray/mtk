9ddce5019cb2a21e50ab354c39c9ef4109a95d76
Adam Murray <adamjmurray@gmail.com>
2011-05-23 20:51:42 -0700

    enhanced Sequencer to handle nil/empty values, and support intervals and pitch classes in the pitch list (see spec for behavior details)

--- lib/mtk/sequencer.rb
+++ lib/mtk/sequencer.rb
@@ -1,27 +1,65 @@
 module MTK
 
-  # An enumerator of {Note}s
+  # An endless enumerator of {Note}s
   class Sequencer
 
     def initialize(pitches, intensities=nil, durations=nil)
       @pitches = pitches.clone
-      @intensities = intensities ? intensities.clone : [Dynamics::mf]
-      @durations = durations ? durations.clone : [1]
+      @intensities = intensities.clone if intensities
+      @durations = durations.clone if durations
       reset
     end
 
     # reset the Sequence to the beginning
     def reset
-      @p_idx = @i_idx = @d_idx = 0
+      @p_idx = @i_idx = @d_idx = -1
+      @pitch = Pitches::C4
+      @intensity = Dynamics::mf
+      @duration = 1
     end
 
     # return next {Note} in sequence
     def next
-      note = Note.new( @pitches[@p_idx], @intensities[@i_idx], @durations[@d_idx] )
-      @p_idx = (@p_idx + 1) % @pitches.length
-      @i_idx = (@i_idx + 1) % @intensities.length
-      @d_idx = (@d_idx + 1) % @durations.length
-      note
+      pitch,intensity,duration = next_pitch,next_intensity,next_duration
+      case pitch
+        when PitchSet then Chord.new(pitch, intensity, duration)
+        else Note.new(pitch, intensity, duration)
+      end
+
     end
+
+    ##################################
+    private
+
+    def next_pitch
+      if @pitches and not @pitches.empty?
+        @p_idx = (@p_idx + 1) % @pitches.length
+        value = @pitches[@p_idx]
+        @pitch = case value
+          when Pitch,PitchSet then value
+          when PitchClass then @pitch + @pitch.pitch_class.distance_to(value)
+          when Numeric then @pitch + value
+          else @pitch
+        end
+      end
+      @pitch
+    end
+
+    def next_intensity
+      if @intensities and not @intensities.empty?
+        @i_idx = (@i_idx + 1) % @intensities.length
+        @intensity = @intensities[@i_idx] || @intensity
+      end
+      @intensity
+    end
+
+    def next_duration
+      if @durations and not @durations.empty?
+        @d_idx = (@d_idx + 1) % @durations.length
+        @duration = @durations[@d_idx] || @duration
+      end
+      @duration
+    end
+
   end
 end
--- spec/mtk/sequencer_spec.rb
+++ spec/mtk/sequencer_spec.rb
@@ -2,6 +2,10 @@ require 'spec_helper'
 
 describe MTK::Sequencer do
 
+  def note(pitch, intensity=mf, duration=1)
+    Note.new pitch,intensity,duration
+  end
+
   describe "#next" do
     it "iterates through the pitch, intenisty, and duration list in parallel to emit Notes" do
       sequencer = Sequencer.new [C4, D4, E4], [p, f], [1,2,3,4]
@@ -11,6 +15,13 @@ describe MTK::Sequencer do
       sequencer.next.should == Note.new(C4, f, 4)
       sequencer.next.should == Note.new(D4, p, 1)
       sequencer.next.should == Note.new(E4, f, 2)
+    end
+
+    it "defaults to Pitch 'C4' when no pitches are given" do
+      sequencer = Sequencer.new [], [p,f], [1,2,3]
+      sequencer.next.should == Note.new(C4, p, 1)
+      sequencer.next.should == Note.new(C4, f, 2)
+      sequencer.next.should == Note.new(C4, p, 3)
     end
 
     it "defaults to intensity 'mf' when no intensities are given" do
@@ -26,6 +37,44 @@ describe MTK::Sequencer do
       sequencer.next.should == Note.new(D4, f, 1)
       sequencer.next.should == Note.new(E4, p, 1)
     end
+
+    it "users the previous pitch/intensity/duration when it encounters a nil value" do
+      sequencer = Sequencer.new [C4, D4, E4, F4, nil], [mp, mf, f, nil], [1, 2, nil]
+      sequencer.next.should == Note.new(C4, mp, 1)
+      sequencer.next.should == Note.new(D4, mf, 2)
+      sequencer.next.should == Note.new(E4, f, 2)
+      sequencer.next.should == Note.new(F4, f, 1)
+      sequencer.next.should == Note.new(F4, mp, 2)
+      sequencer.next.should == Note.new(C4, mf, 2)
+    end
+
+    it "adds Numeric intervals in the pitch list to the previous pitch" do
+      sequencer = Sequencer.new [C4, 1, 2, 3]
+      sequencer.next.should == note(C4)
+      sequencer.next.should == note(C4+1)
+      sequencer.next.should == note(C4+1+2)
+      sequencer.next.should == note(C4+1+2+3)
+      sequencer.next.should == note(C4)
+    end
+
+    it "goes to the nearest Pitch for any PitchClasses in the pitch list" do
+      sequence = Sequencer.new [C4, F, C, G, C]
+      sequence.next.should == note(C4)
+      sequence.next.should == note(F4)
+      sequence.next.should == note(C4)
+      sequence.next.should == note(G3)
+      sequence.next.should == note(C4)
+    end
+
+    it "does not endlessly ascend or descend when alternating between two pitch classes a tritone apart" do
+      sequence = Sequencer.new [C4, Gb, C, Gb, C]
+      sequence.next.should == note(C4)
+      sequence.next.should == note(Gb4)
+      sequence.next.should == note(C4)
+      sequence.next.should == note(Gb4)
+      sequence.next.should == note(C4)
+    end
+
   end
 
   describe "#reset" do
