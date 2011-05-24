ebe8c04b898917c81a364922ff04696020f59ac5
Adam Murray <adamjmurray@gmail.com>
2011-05-24 09:26:20 -0700

    reworked midi file interface: - merged MIDI::FileReader and FileWriter into MIDI::File - renamed the methods for clarity - added support for writing multi track files via #write_timelines - cleaned up Module name collision funkiness by using ::ModuleName to reference same-named modules outside of the MTK library - added the convenience function MTK::MIDI_File() to construct MTK::MIDI::File objects

---
+++ lib/mtk/midi/file.rb
@@ -0,0 +1,179 @@
+require 'rubygems'
+require 'midilib'
+
+module MTK
+  module MIDI
+
+    class File
+      def initialize file
+        if file.respond_to? :path
+          @file = file.path
+        else
+          @file = file.to_s
+        end
+      end
+
+      # Read a MIDI file into an Array of {Timeline}s
+      #
+      # @param filepath [String, #path] path of the file to be written
+      # @return [Timeline]
+      #
+      def to_timelines
+        timelines = []
+
+        ::File.open(@file, 'rb') do |f|
+          sequence = ::MIDI::Sequence.new
+          sequence.read(f)
+          pulses_per_beat = sequence.ppqn.to_f
+          track_idx = -1
+
+          for track in sequence
+            track_idx += 1
+            timeline = Timeline.new
+            note_ons = {}
+            #puts "TRACK #{track_idx}"
+
+            for event in track
+              #puts "#{event.class}: #{event}   @#{event.time_from_start}"
+              case event
+                when ::MIDI::NoteOn
+                  note_ons[event.note] = event
+
+                when ::MIDI::NoteOff
+                  if on_event = note_ons.delete(event.note)
+                    time = (on_event.time_from_start)/pulses_per_beat
+                    duration = (event.time_from_start - on_event.time_from_start)/pulses_per_beat
+                    note = Note.from_midi event.note, on_event.velocity, duration
+                    timeline.add time, note
+                  end
+              end
+            end
+            timelines << timeline
+          end
+        end
+        timelines
+      end
+
+      def write(anything)
+        case anything
+          when Timeline then
+            write_timeline(anything)
+          when Array then
+            write_timelines(anything)
+          else
+            raise "#{self.class}#write doesn't understand #{anything.class}"
+        end
+      end
+
+      def write_timelines(timelines, parent_sequence=nil)
+        sequence = parent_sequence || ::MIDI::Sequence.new
+        for timeline in timelines
+          write_timeline(timeline, sequence)
+        end
+        write_to_disk sequence unless parent_sequence
+      end
+
+      # Write the Timeline as a MIDI file
+      #
+      # @param [Timeline]
+      def write_timeline(timeline, parent_sequence=nil)
+        sequence = parent_sequence || ::MIDI::Sequence.new
+        clock_rate = sequence.ppqn
+        track = add_track sequence
+        channel = 1
+
+        for time, event in timeline
+          time *= clock_rate
+          case event
+            when Note
+              pitch, velocity = event.pitch, event.velocity
+              add_event track, time => note_on(channel, pitch, velocity)
+              duration = event.duration_in_pulses(clock_rate)
+              add_event track, time+duration => note_off(channel, pitch, velocity)
+
+            when Chord
+              velocity = event.velocity
+              duration = event.duration_in_pulses(clock_rate)
+              for pitch in event.pitches
+                pitch = pitch.to_i
+                add_event track, time => note_on(channel, pitch, velocity)
+                add_event track, time+duration => note_off(channel, pitch, velocity)
+              end
+
+          end
+        end
+        track.recalc_delta_from_times
+
+        write_to_disk sequence unless parent_sequence
+      end
+
+
+      ########################
+      private
+
+      def write_to_disk(sequence)
+        ::File.open(@file, 'wb') { |f| sequence.write f }
+      end
+
+      def print_midi sequence
+        for track in sequence
+          puts "\n*** track \"#{track.name}\""
+          puts "#{track.events.length} events"
+          for event in track
+            puts "#{event.to_s} (#{event.time_from_start})"
+          end
+        end
+      end
+
+      # Set tempo in terms of Quarter Notes per Minute (aka BPM)
+      def tempo(bpm)
+        ms_per_quarter_note = ::MIDI::Tempo.bpm_to_mpq(bpm)
+        ::MIDI::Tempo.new(ms_per_quarter_note)
+      end
+
+      def program(program_number)
+        ::MIDI::ProgramChange.new(channel, program_number)
+      end
+
+      def note_on(channel, pitch, velocity)
+        ::MIDI::NoteOn.new(channel, pitch.to_i, velocity)
+      end
+
+      def note_off(channel, pitch, velocity)
+        ::MIDI::NoteOff.new(channel, pitch.to_i, velocity)
+      end
+
+      def cc(channel, controller, value)
+        ::MIDI::Controller.new(channel, controller.to_i, value.to_i)
+      end
+
+      def pitch_bend(channel, value)
+        ::MIDI::PitchBend.new(channel, value)
+      end
+
+      def add_track sequence, opts={}
+        track = ::MIDI::Track.new(sequence)
+        track.name = opts.fetch :name, ''
+        sequence.tracks << track
+        track
+      end
+
+      def add_event track, event_hash
+        for time, event in event_hash
+          event.time_from_start = time
+          track.events << event
+          event
+        end
+      end
+
+    end
+
+  end
+
+  def MIDI_File(f)
+    MIDI::File.new(f)
+  end
+  module_function :MIDI_File
+
+end
+
---
+++ spec/mtk/midi/file_spec.rb
@@ -0,0 +1,208 @@
+require 'mtk/midi/file'
+require 'tempfile'
+
+describe MTK::MIDI::File do
+
+  let(:test_mid) { File.join(File.dirname(__FILE__), '..', '..', 'test.mid') }
+
+  def tempfile
+    @tempfile ||= Tempfile.new 'MTK-midi_file_writer_spec'
+  end
+
+  after do
+    if @tempfile
+      @tempfile.close
+      @tempfile.unlink
+    end
+  end
+
+  def note_ons_and_offs(track)
+    note_ons, note_offs = [], []
+    for event in track.events
+      note_ons << event if event.is_a? MIDI::NoteOn
+      note_offs << event if event.is_a? MIDI::NoteOff
+    end
+    return note_ons, note_offs
+  end
+
+  describe "#to_timelines" do
+    it "converts a single-track MIDI file to an Array containing one Timeline" do
+      MIDI_File(test_mid).to_timelines.length.should == 1 # one track
+    end
+
+    it "converts note on/off messages to Note events" do
+      MIDI_File(test_mid).to_timelines.first.should == {
+        0.0 => [Note.new(C4, 126/127.0, 0.25)],
+        1.0 => [Note.new(Db4, 99/127.0, 0.5)],
+        2.0 => [Note.new(D4, 72/127.0, 0.75)],
+        3.0 => [Note.new(Eb4, 46/127.0, 1.0), Note.new(E4, 46/127.0, 1.0)]
+      }
+    end
+  end
+
+  describe "#write_timeline" do
+    it 'writes Notes in a Timeline to a MIDI file' do
+      MIDI_File(tempfile).write_timeline(
+        Timeline.from_hash({
+          0 => Note.new(C4, 0.7, 1),
+          1 => Note.new(G4, 0.8, 1),
+          2 => Note.new(C5, 0.9, 1)
+        })
+      )
+
+      # Now let's parse the file and check some expectations
+      File.open(tempfile.path, 'rb') do |file|
+        seq = MIDI::Sequence.new
+        seq.read(file)
+        seq.tracks.size.should == 1
+
+        track = seq.tracks[0]
+        note_ons, note_offs = note_ons_and_offs(track)
+        note_ons.length.should == 3
+        note_offs.length.should == 3
+
+        note_ons[0].note.should == C4.to_i
+        note_ons[0].velocity.should be_within(0.5).of(127*0.7)
+        note_offs[0].note.should == C4.to_i
+        note_ons[0].time_from_start.should == 0
+        note_offs[0].time_from_start.should == 480
+
+        note_ons[1].note.should == G4.to_i
+        note_ons[1].velocity.should be_within(0.5).of(127*0.8)
+        note_offs[1].note.should == G4.to_i
+        note_ons[1].time_from_start.should == 480
+        note_offs[1].time_from_start.should == 960
+
+        note_ons[2].note.should == C5.to_i
+        note_ons[2].velocity.should be_within(0.5).of(127*0.9)
+        note_offs[2].note.should == C5.to_i
+        note_ons[2].time_from_start.should == 960
+        note_offs[2].time_from_start.should == 1440
+      end
+    end
+
+    it 'writes Chords in a Timeline to a MIDI file' do
+      MIDI_File(tempfile).write_timeline(
+        Timeline.from_hash({
+          0 => Chord.new([C4, E4], 0.5, 1),
+          2 => Chord.new([G4, B4, D5], 1, 2)
+        })
+      )
+
+      # Now let's parse the file and check some expectations
+      File.open(tempfile.path, 'rb') do |file|
+        seq = MIDI::Sequence.new
+        seq.read(file)
+        seq.tracks.size.should == 1
+
+        track = seq.tracks[0]
+        note_ons, note_offs = note_ons_and_offs(track)
+        note_ons.length.should == 5
+        note_offs.length.should == 5
+
+        note_ons[0].note.should == C4.to_i
+        note_offs[0].note.should == C4.to_i
+        note_ons[0].velocity.should == 64
+        note_ons[0].time_from_start.should == 0
+        note_offs[0].time_from_start.should == 480
+
+        note_ons[1].note.should == E4.to_i
+        note_offs[1].note.should == E4.to_i
+        note_ons[1].velocity.should == 64
+        note_ons[1].time_from_start.should == 0
+        note_offs[1].time_from_start.should == 480
+
+        note_ons[2].note.should == G4.to_i
+        note_offs[2].note.should == G4.to_i
+        note_ons[2].velocity.should == 127
+        note_ons[2].time_from_start.should == 960
+        note_offs[2].time_from_start.should == 1920
+
+        note_ons[3].note.should == B4.to_i
+        note_offs[3].note.should == B4.to_i
+        note_ons[3].velocity.should == 127
+        note_ons[3].time_from_start.should == 960
+        note_offs[3].time_from_start.should == 1920
+
+        note_ons[4].note.should == D5.to_i
+        note_offs[4].note.should == D5.to_i
+        note_ons[4].velocity.should == 127
+        note_ons[4].time_from_start.should == 960
+        note_offs[4].time_from_start.should == 1920
+      end
+    end
+  end
+
+  describe "#write_timelines" do
+    it "writes a multitrack MIDI file" do
+      MIDI_File(tempfile).write_timelines([
+        Timeline.from_hash({
+          0 => Note.new(C4, 0.7, 1),
+          1 => Note.new(G4, 0.8, 1),
+        }),
+        Timeline.from_hash({
+          1 => Note.new(C5, 0.9, 2),
+          2 => Note.new(D5, 1, 2),
+        }),
+      ])
+
+      # Now let's parse the file and check some expectations
+      File.open(tempfile.path, 'rb') do |file|
+        seq = MIDI::Sequence.new
+        seq.read(file)
+        seq.tracks.size.should == 2
+
+        track = seq.tracks[0]
+        note_ons, note_offs = note_ons_and_offs(track)
+        note_ons.length.should == 2
+        note_offs.length.should == 2
+
+        note_ons[0].note.should == C4.to_i
+        note_ons[0].velocity.should be_within(0.5).of(127*0.7)
+        note_offs[0].note.should == C4.to_i
+        note_ons[0].time_from_start.should == 0
+        note_offs[0].time_from_start.should == 480
+
+        note_ons[1].note.should == G4.to_i
+        note_ons[1].velocity.should be_within(0.5).of(127*0.8)
+        note_offs[1].note.should == G4.to_i
+        note_ons[1].time_from_start.should == 480
+        note_offs[1].time_from_start.should == 960
+
+        track = seq.tracks[1]
+        note_ons, note_offs = note_ons_and_offs(track)
+        note_ons.length.should == 2
+        note_offs.length.should == 2
+
+        note_ons[0].note.should == C5.to_i
+        note_ons[0].velocity.should be_within(0.5).of(127*0.9)
+        note_offs[0].note.should == C5.to_i
+        note_ons[0].time_from_start.should == 480
+        note_offs[0].time_from_start.should == 1440
+
+        note_ons[1].note.should == D5.to_i
+        note_ons[1].velocity.should == 127
+        note_offs[1].note.should == D5.to_i
+        note_ons[1].time_from_start.should == 960
+        note_offs[1].time_from_start.should == 1920
+      end
+    end
+  end
+
+  describe "#write" do
+    it "calls write_timeline when given a Timeline" do
+      midi_file = MIDI_File(nil)
+      timeline = Timeline.new
+      midi_file.should_receive(:write_timeline).with(timeline)
+      midi_file.write(timeline)
+    end
+
+    it "calls write_timelines when given an Array" do
+      midi_file = MIDI_File(nil)
+      timelines = [Timeline.new]
+      midi_file.should_receive(:write_timelines).with(timelines)
+      midi_file.write(timelines)
+    end
+  end
+
+end
