0966088e5087fec2764f8785f2a2b38b7f0b86ca
Adam Murray <adamjmurray@gmail.com>
2011-06-27 23:04:39 -0700

    added max_elements limit to AbstractPattern, also added #element_count [delivers #15093815]

--- lib/mtk/pattern/abstract_pattern.rb
+++ lib/mtk/pattern/abstract_pattern.rb
@@ -1,35 +1,68 @@
 module MTK
   module Pattern
 
-    # A finite list of elements, which can be enumerated one at a time.
+    # A pattern of elements that can be emitted one element at a time.
+    #
+    # @abstract subclass and override {#advance!} and {#current} to implement a Pattern
+    #
     class AbstractPattern
       include Enumerator
 
+      # The elements in the pattern
       attr_reader :elements
 
       # The type of elements in the pattern, such as :pitch, :intensity, or :duration
+      #
       # This is often needed by {Sequencer} classes to interpret the pattern elements.
       attr_reader :type
+
+      # The number of elements emitted since the last {#rewind}
+      attr_reader :element_count
       
+      # The maximum number of elements this Pattern will emit before a StopIteration exception
+      attr_reader :max_elements
+
+      # @param elements [Array] the list of elements in the pattern
+      # @param options [Hash] the pattern options
+      # @option options [String] :type the pattern {#type}
+      # @option options [Fixnum] :max_elements the {#max_elements}
       def initialize(elements, options={})
         @elements = elements
         @options = options
         @type = options[:type]
+        @max_elements = options[:max_elements]
         rewind
       end
 
-      def self.from_a(enumerable)
-        new(enumerable)
+      # Construct a pattern from an Array.
+      # @param elements [Array] the list of elements in the pattern
+      # @see #initialize
+      def self.from_a(elements)
+        new(elements.to_a)
       end
 
-      # Return the next value in the sequence
+      # Reset the pattern to the beginning
+      def rewind
+        @current = nil
+        @element_count = 0
+        self
+      end
+
+      # Emit the next element in the pattern
+      # @raise StopIteration when the pattern has emitted all values, or has hit the {#max_elements} limit.
       def next
         if @current.is_a? Enumerator
+          subpattern_has_next = true
           begin
-            return @current.next
+            subpattern_next = @current.next
           rescue StopIteration
-            # continue on and advance
+            subpattern_has_next = false
           end
+          if subpattern_has_next
+            increment_element_count!
+            return subpattern_next
+          end
+          # else fall through and continue with normal behavior
         end
 
         begin
@@ -45,14 +78,10 @@ module MTK
           return self.next
         end
 
+        increment_element_count!
         @current
       end
 
-      # Reset the sequence to the beginning
-      def rewind
-        @current = nil
-        self
-      end
 
       ##################
       protected
@@ -63,6 +92,15 @@ module MTK
 
       def current
         @elements
+      end
+
+
+      ##################
+      private
+
+      def increment_element_count!
+        @element_count += 1
+        raise StopIteration if @max_elements and @element_count > @max_elements
       end
     end
 
--- spec/mtk/pattern/abstract_pattern_spec.rb
+++ spec/mtk/pattern/abstract_pattern_spec.rb
@@ -10,4 +10,20 @@ describe MTK::Pattern::AbstractPattern do
     end
   end
 
+  describe "#max_elements" do
+    it "is the :max_elements option the pattern was constructed with" do
+      PATTERN.new([], :max_elements => 1).max_elements.should == 1
+    end
+
+    it "is nil by default" do
+      PATTERN.new([]).max_elements.should be_nil
+    end
+
+    it "causes a StopIteration exception after the number of elements have been emitted" do
+      cycle = PATTERN.new([:anything], :max_elements => 5)
+      5.times { cycle.next }
+      lambda { cycle.next }.should raise_error
+    end
+  end
+
 end
