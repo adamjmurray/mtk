74f7741c54a8a87e8bd5659b87535aabfcbfb92c
Adam Murray <adamjmurray@gmail.com>
2011-06-30 09:42:16 -0700

    some more tests for the grammar

--- spec/mtk/lang/grammar_spec.rb
+++ spec/mtk/lang/grammar_spec.rb
@@ -8,12 +8,45 @@ describe MTK::Lang::Grammar do
   end
 
   describe ".parse" do
+
+    it "should parse pitch sequences" do
+      parse("C4 D4 E4", :pitch_sequence).should == Pattern.PitchSequence(C4, D4, E4)
+    end
+
     it "should parse pitch classes" do
       parse("Db", :pitch_class).should == Db
     end
 
-    it do
-      parse("C4 D4 E4", :pitch_sequence).should == Pattern.PitchSequence(C4, D4, E4)
+    it "should parse ints as numbers" do
+      parse("123", :number).should == 123
+    end
+
+    it "should parse floats as numbers" do
+      parse("1.23", :number).should == 1.23
+    end
+
+    it "should parse floats" do
+      parse("1.23", :float).should == 1.23
+    end
+
+    it "should parse negative floats" do
+      parse("-1.23", :float).should == -1.23
+    end
+
+    it "should parse ints" do
+      parse("123", :int).should == 123
+    end
+
+    it "should parse negative ints" do
+      parse("-123", :int).should == -123
+    end
+
+    it "should parse negative ints" do
+      parse("-123", :int).should == -123
+    end
+
+    it "should give nil as the value for whitespace" do
+      parse(" \t\n", :space).should == nil
     end
   end
 end
