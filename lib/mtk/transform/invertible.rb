ee00910a9d79c38afd2f43a143d073fb076ff996
Adam Murray <adamjmurray@gmail.com>
2011-07-04 22:48:28 -0700

    fixed the doc comments in Transform::Invertible

--- lib/mtk/transform/invertible.rb
+++ lib/mtk/transform/invertible.rb
@@ -1,11 +1,11 @@
 module MTK::Transform
 
-  # {Mappable} class whose elements can handle the :transpose message
+  # {Mappable} class whose elements can handle the :invert message
   # @note Classes including this module should include either MTK::Collection or provide a #first method
   module Invertible
 
-    # Transpose all elements upward by the given interval
-    # @param interval_in_semitones [Numeric] an interval in semitones
+    # Invert all elements around the given inversion point
+    # @param inversion_point [Numeric] the value around which all elements will be inverted (defaults to the first element in the collection)
     def invert(inversion_point=first)
       map{|elem| elem.invert(inversion_point) }
     end
