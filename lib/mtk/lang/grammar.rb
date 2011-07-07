fc1c1df24a62d65ceaec66ef07a31c8829b0ef7a
Adam Murray <adamjmurray@gmail.com>
2011-07-07 00:46:16 -0700

    linking to mtk_grammar.citrus using the full path, because the link is not working on rubydoc.info

--- lib/mtk/lang/grammar.rb
+++ lib/mtk/lang/grammar.rb
@@ -4,7 +4,7 @@ Citrus.load File.join(File.dirname(__FILE__),'mtk_grammar')
 module MTK
   module Lang
 
-    # Parser for the {file:mtk_grammar.citrus MTK grammar}
+    # Parser for the {file:lib/mtk/lang/mtk_grammar.citrus MTK grammar}
     class Grammar
 
       def self.parse(syntax, root=:pitch)
