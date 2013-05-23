8385ce786a4ae177c5d2999a11a5050db7a85894
Adam Murray <adamjmurray@gmail.com>
2013-05-22 21:08:52 -0700

    Redefining Cycle as a Sequence that has unlimited max_cycles by default

--- lib/mtk/patterns/cycle.rb
+++ lib/mtk/patterns/cycle.rb
@@ -3,22 +3,13 @@ module MTK
 
     # An endless enumerator that outputs an element one at a time from a list of elements,
     # looping back to the beginning when elements run out.
-    class Cycle < Pattern
-
+    # This is the same as a Sequence but by default has unlimited @max_cycles
+    class Cycle < Sequence
 
       def initialize(elements, options={})
         super
-        @max_cycles = options[:max_cycles] # TODO: default to 1 to avoid accidental infinite loops
-      end
-
-      ###################
-      protected
-
-      # (see Pattern#advance!)
-      def advance!
-        @index += 1
-        raise StopIteration if @index >= @elements.length
-        @current = @elements[@index]
+        # Base Pattern & Sequence default to 1 max_cycle, this defaults to nil which is unlimited cycles
+        @max_cycles = options[:max_cycles]
       end
 
     end
