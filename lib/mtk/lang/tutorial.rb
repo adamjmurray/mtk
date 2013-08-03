2f8f620ae0df8d437d5e358dadff5026c1c42248
Adam Murray <adamjmurray@gmail.com>
2013-08-02 18:39:45 -0700

    more progress on the tutorial system

--- lib/mtk/lang/tutorial.rb
+++ lib/mtk/lang/tutorial.rb
@@ -7,8 +7,7 @@ module MTK
     # @private
     class Tutorial
 
-      def initialize(output)
-        @output = output
+      def initialize
         @current_step_index = 0
 
         @steps = [
@@ -22,52 +21,75 @@ module MTK
               To play a diatonic pitch class, enter one of the following letters
               (upper or lower case is allowed):
 
-              C D E F G A B
+              C D E F G A B   c d e f g a b
               ",
-            validate: lambda{|input| input =~ /^\s*[A-G]\s*$/i },
+            validation: /^[A-G]$/i,
           },
           {
-              title: 'Pitch Classes (chromatic)',
-              description: "
-              To play a diatonic pitch class, enter any of the following letters: C D E F G A B
-              (they can be upper-case or lower-case)
+            title: 'Pitch Classes (chromatic)',
+            description: "
+              The chromatic pitch classes are the 12 white or black keys on a piano in a
+              given octave. They form the basis of all 'western' music theory.
+
+              To play a chromatic pitch class, enter any of the 7 diatonic pitch classes
+              immediately followed by 0, 1, or 2 flats (b) or sharps (#). Each flat (b)
+              lowers the pitch by a semitone and each sharp (#) raises by a semitone.
+
+              Here are some examples (Note, upper or lower case is allowed for the diatonic
+              pitch class but flats must be lower case):
+
+              C# Eb F Gbb A## B   c# eb f gbb a## b
               ",
-              validate: lambda{|input| input =~ /^\s*[A-G]\s*$/i },
-              success:  lambda{|input| play input },
-              failure:  lambda{|input| "Invalid entry: #{input}"}
+            validation: :pitch_class,
           },
           {
               title: 'Pitches',
               description: "
-                To play a pitch, enter a pitch class immediate following by an octave number
-                For example, C4 is middle C. C-1 is the lowest note available, and G9 is the highest.
+                To play a pitch, enter a (chromatic) pitch class immediate following by an
+                octave number.
+
+                There is no universal standard numbering scheme for octaves in music.
+                This library uses \"scientific pitch notation\", which defines
+                C4 as \"Middle C\" and A4 as the standard tuning pitch A440.
+
+                In this library, C-1 is the lowest note available and G9 is the highest,
+                corresponding to MIDI pitch values 0 and 127. If you try to play a pitch
+                outside this range, it will be mapped to the closest available pitch.
+
+                Here are some examples:
+
+                G3 eb4 F#5 B-1 C##9 dbb6
                 ",
-              validate: lambda{},
-              success: lambda{},
-              failure: lambda{}
+              validation: :pitch,
           },
           {
               title: 'Sequences',
               description: "
-                To play a sequence of pitches (or pitch classes), simply list them with spaces in between
-                For example, C4 is middle C. C-1 is the lowest note available, and G9 is the highest.
+                To play a sequence of pitches or pitch classes, simply list them with spaces in between.
+                Pitches and pitch classes may be interchanged in a given sequence. Any pitch class will
+                output a pitch closest to the previous pitch (starting from C4 at the beginning of the sequence).
+
+                Here is an example (Note, unlike previous tutorials, enter the entire line):
+
+                c5 c g5 g a a g
                 ",
-              validate: lambda{},
-              success: lambda{},
-              failure: lambda{}
+              validation: :bare_sequence,
           },
 
-        ].map{|step_options| TutorialStep.new(self, step_options) }
+        ].map{|step_options| TutorialStep.new(step_options) }
       end
 
 
-      def run
+      def run(output)
         puts
+        puts TutorialStep::SEPARATOR
         puts "Welcome to the MTK tutorial"
-        puts "==========================="
         puts
-        ensure_output
-        loop{ select_step.run }
+        puts "This is a work in progress. Check back in future versions for more tutorials."
+        puts
+        output = ensure_output(output)
+        loop{ select_step.run(output) }
+
       rescue SystemExit, Interrupt
         puts
         puts
@@ -84,21 +106,28 @@ module MTK
 
       def select_step
         puts
+        puts TutorialStep::SEPARATOR
         puts "Tutorials"
         puts "---------"
 
+        all_done = @current_step_index >= @steps.length
         step = nil
         while step == nil
           puts toc
           puts
+          puts "You've completed the last tutorial!\n\n" if all_done
           puts "Press Ctrl+C to exit at any time."
-          puts "Select a tutorial number, or hit enter to go to the next one (indicated by =>)."
+          print "Select a tutorial number, or hit enter to "
+          puts (if all_done then "exit:" else "go to the next one (indicated by =>):" end)
 
           input = gets.strip
           step_index = case input
-            when /^\d+$/ then input.to_i - 1
-            when '' then @current_step_index
-            else nil
+            when /^\d+$/
+              input.to_i - 1
+            when ''
+              if all_done then raise SystemExit.new else @current_step_index end
+            else
+              nil
           end
 
           step = @steps[step_index] if step_index and step_index >= 0
@@ -111,33 +140,22 @@ module MTK
       end
 
 
-      def play(mtk_syntax)
-        sequencer = MTK::Lang::Parser.parse(mtk_syntax)
-        if sequencer
-          @output.play sequencer.to_timeline
-        else
-          STDERR.puts "Nothing to play for \"#{mtk_syntax}\""
-        end
-      rescue Citrus::ParseError
-        STDERR.puts $!
-      end
-
-
       #####################################################
       private
 
-      def ensure_output
-        if @output
-          puts "Using \"#{@output.name}\" for output"
+      def ensure_output(output)
+        if output
+          puts "Using \"#{output.name}\" for output."
         else
-          fail "--output option must be given"
+          puts "ERROR: --output option must be given when launching the tutorial."
+          exit 1
           # TODO: select an output, explain --output option...
           # Or auto-select?
           #if defined? MTK::IO::DLSSynthDevice
           #  puts "It looks like you are on OS X so we'll try using the built-in 'DLS' synthesizer"
           #end
         end
-
+        output
       end
 
     end
--- lib/mtk/lang/tutorial_step.rb
+++ lib/mtk/lang/tutorial_step.rb
@@ -4,69 +4,77 @@ module MTK
     # @private
     class TutorialStep
 
+      SEPARATOR = "================================================================================\n\n"
+
+
       # Every TutorialStep requires the options at construction title:
       #   title - The short summary displayed in the tutorial's table of contents.
       #   description - The full description displayed when entering the tutorial step.
       #   validate(input) - Validate user input entered after the description is displayed.
       #   success(input) - Perform an action on successful validation
       #   failure(input) - Instruct the user on failed validation
-      def initialize tutorial, options
-        @tutorial = tutorial # handles playing output
-
-        for attr in [:title, :description, :validate]
-          instance_variable_set "@#{attr}", ( options[attr] || fail("#{attr} required in #{self.class}(#{options})") )
-        end
-        for attr in [:validate, :success, :failure]
-          fail "#{attr} must be a Proc in #{self.class}(#{options})" unless options[attr].is_a? Proc or options[attr].nil?
-        end
-
-        @description = @description.split("\n").map{|line| line.strip }.join("\n") # trim extra whitespace
+      def initialize options
+        @title = options[:title]
+        @description = options[:description].split("\n").map{|line| line.strip }.join("\n") # trim extra whitespace
+        @validation = options[:validation]
       end
 
 
-      def run
-        puts "================================================================="
-        puts
+      def run(output)
+        puts SEPARATOR
         puts "Tutorial: #{@title}"
         puts @description
         puts
-        puts "Try it now:"
+        print "Try it now: "
 
         did_it_once = false
         input = gets.strip
         until did_it_once and input.empty?
-          until @validate[input]
+          until validate(input)
             failure(input)
-            puts "Try again:"
+            print "Try again: "
             input = gets.strip
           end
 
-          success(input)
+          success(input, output)
           did_it_once = true
           puts
-          puts "Good!"
-          puts "Try again, or hit enter to exit this tutorial:"
+          puts "Good! Try again, or hit enter to exit this tutorial:"
           input = gets.strip
         end
       end
 
 
-      def success(input)
-        if @success
-          @success.call(input)
-        else
-          @tutorial.play(input)
+      def validate(input)
+        return false if input.empty?
+
+        case @validation
+          when Symbol
+            MTK::Lang::Parser.parse(input, @validation)
+            true
+          else # Assume Regexp
+            (input =~ /^[A-G]$/i) != nil
         end
+      rescue Citrus::ParseError
+        false
+      end
+
+
+      def success(input, output)
+        sequencer = MTK::Lang::Parser.parse(input)
+        if sequencer
+          output.play sequencer.to_timeline
+        else
+          STDERR.puts "Nothing to play for \"#{input}\""
+        end
+      rescue Citrus::ParseError
+        STDERR.puts $!
       end
 
 
       def failure(input)
-        if @failure
-          @failure.call(input)
-        else
-          puts
-          puts "Invalid entry \"#{input}\""
-        end
+        puts
+        puts "Invalid entry \"#{input}\""
       end
 
 
