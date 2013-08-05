c3887c2a8c2276bc1f51cefb9c7d770fa7d8603e
Adam Murray <adamjmurray@gmail.com>
2013-08-04 18:13:23 -0700

    added tests for the "accidental" (flats and sharps) grammar rule

--- spec/mtk/lang/parser_spec.rb
+++ spec/mtk/lang/parser_spec.rb
@@ -475,6 +475,30 @@ describe MTK::Lang::Parser do
     end
 
 
+    context 'accidental rule' do
+      it "parses a single flat 'b'" do
+        lambda{ parse('b', :accidental) }.should_not raise_error
+      end
+
+      it "parses a double flat 'bb'" do
+        lambda{ parse('bb', :accidental) }.should_not raise_error
+      end
+
+      it "parses a single sharp '#'" do
+        lambda{ parse('#', :accidental) }.should_not raise_error
+      end
+
+      it "parses a double sharp '##'" do
+        lambda{ parse('##', :accidental) }.should_not raise_error
+      end
+
+      it "doesn't parse trip flats or sharps" do
+        lambda{ parse('bbb', :accidental) }.should raise_error
+        lambda{ parse('###', :accidental) }.should raise_error
+      end
+    end
+
+
     context 'interval rule' do
       it "parses intervals" do
         for interval_name in Interval::ALL_NAMES
